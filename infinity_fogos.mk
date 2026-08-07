113ae9f829dcfaf3de833830fae880ccdac9ce2a
shipukacapri <242039583+shipukacapri@users.noreply.github.com>
2026-08-07 18:05:53 +0530

    fogos: InfinityX Bringup

--- infinity_fogos.mk
+++ infinity_fogos.mk
@@ -11,10 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fogos device
 $(call inherit-product, device/motorola/fogos/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common InfinityX stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fogos
+# Infinity Flags
+INFINITY_MAINTAINER := Shipu
+
+PRODUCT_NAME := infinity_fogos
 PRODUCT_DEVICE := fogos
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_BRAND := motorola
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# InfinityX Build Setup for Moto G34 (fogos)
+for variant in user userdebug eng; do
+    add_lunch_combo infinity_fogos-$variant
+done
